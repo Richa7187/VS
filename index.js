9e17484852612dbbb6aa33172bea2d4c52f50323
Richa Sharma <richasharma9577@gmail.com>
2023-03-05 05:47:52 +0530

    reverted

--- index.js
+++ index.js
@@ -5,26 +5,24 @@ $(document).ready(function() {
     
     //function for Shuffle button
     $('#Shuffle').on('click',function () {
-        //$('.cardContainer > div').each(function () {
-        //    let randomOrder = Math.floor(Math.random()*9)+1;
-        //    $(this).css('order',randomOrder);
-        //})
-        let shuffledArray = [...arr];
-        for (let i = 0; i < noOfCards; i++) {
-            const j = Math.floor(Math.random() * noOfCards);
-            [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
-        }
-        console.log(shuffledArray);
-
-        arrangeCards(shuffledArray);
+        $('.cardContainer > div').each(function () {
+            let randomOrder = Math.floor(Math.random()*9)+1;
+            $(this).css('order',randomOrder);
+        })
+        //let shuffledArray = [...arr];
+        //for (let i = 0; i < noOfCards; i++) {
+        //    const j = Math.floor(Math.random() * noOfCards);
+        //    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+        //}
+        //arrangeCards(shuffledArray);
     })
 
     //function for Sort button
     $('#Sort').on('click',function () {
-        //$('.cardContainer > div').each(function () {
-        //    $(this).removeAttr('style');
-        //})
-        arrangeCards(arr);
+        $('.cardContainer > div').each(function () {
+            $(this).removeAttr('style');
+        })
+        //arrangeCards(arr);
     })
 
     function arrangeCards(arr) {
